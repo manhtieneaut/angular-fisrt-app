ccffff554760cdabde2cd3a332895cd32c3c3a72
Nguyen Manh Tien (CTV-CNTT) <tiennm1@kienlongbank.com>
2025-05-26 13:41:46 +0700

    start new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule
-  ],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-app';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,5 @@
 import { Routes } from '@angular/router';
-import { ProductComponent } from './pages/product/product.component';
-import { UserComponent } from './pages/user/user.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
 
 export const routes: Routes = [
-{
-    path: '',
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'user', component: UserComponent },
-      { path: 'product', component: ProductComponent},
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-    ]
-  }
+
 ];
