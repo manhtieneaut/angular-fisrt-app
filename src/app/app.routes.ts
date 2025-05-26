d0105cc155e4895f983ab196313ce4df81f4062d
Nguyen Manh Tien (CTV-CNTT) <tiennm1@kienlongbank.com>
2025-05-26 15:14:59 +0700

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { ProductComponent } from './layout/product/product.component';
+import { SettingComponent } from './layout/setting/setting.component';
+import { ReportComponent } from './layout/report/report.component';
+
 
 export const routes: Routes = [
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },  // route mặc định
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'product', component: ProductComponent },
+  { path: 'setting', component: SettingComponent },
+  { path: 'report', component: ReportComponent },
+
 
 ];
