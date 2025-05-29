4f798ef128cd0c4895ad73a530b6b64e72e69d38
Nguyen Manh Tien (CTV-CNTT) <tiennm1@kienlongbank.com>
2025-05-29 11:00:38 +0700

    mat tab group

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    MatTabsModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
